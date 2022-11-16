d4cb879f92b0499faa9c6c54beb20986e40a7b14
Henry de Jongh <henry00@live.nl>
2022-11-16 22:31:31 +0100

    Massive raycaster speedup.

--- Scripts/Editor/VertexTracer.cs
+++ Scripts/Editor/VertexTracer.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -87,15 +86,6 @@ namespace AlpacaIT.VertexTracer
                 }
             }
 
-            //Texture2D lightmap = new Texture2D(lightmapSize, lightmapSize, TextureFormat.RGBA32, false);
-            //lightmap.filterMode = FilterMode.Point;
-            //lightmap.wrapMode = TextureWrapMode.Clamp;
-            //lightmap.SetPixels(pixels);
-            //lightmap.Apply();
-
-            //var renderTexture = new RenderTexture(256, 256, 24);
-            //RenderTexture.active = renderTexture;
-
             var renderer = meshFilter.GetComponent<MeshRenderer>();
             renderer.sharedMaterial = new Material(renderer.sharedMaterial);
             renderer.sharedMaterial.name = "Vertex Tracer Material";
@@ -105,31 +95,6 @@ namespace AlpacaIT.VertexTracer
                 lightmap = renderer.gameObject.AddComponent<Lightmap>();
             lightmap.resolution = lightmapSize;
             lightmap.pixels = pixels;
-            //RenderTexture.active = null;
-            //renderTexture.Release();
-
-            /*
-            var vertices = meshBuilder.worldVertices;
-            var triangles = meshBuilder.meshTriangles;
-
-            var colors = new Color[vertices.Count];
-
-            for (int i = 0; i < triangles.Count; i += 3)
-            {
-                var v1 = vertices[triangles[i]];
-                var v2 = vertices[triangles[i] + 1];
-                var v3 = vertices[triangles[i] + 2];
-
-                var t2 = Time.realtimeSinceStartup;
-                var res = Raycast(v1, v2, v3);
-                tracingTime += Time.realtimeSinceStartup - t2;
-
-                colors[triangles[i]] = res.c1;
-                colors[triangles[i + 1]] = res.c2;
-                colors[triangles[i + 2]] = res.c3;
-            }
-
-            mesh.colors = colors;*/
         }
 
         private static void SetPixel(ref uint[] pixels, int x, int y, uint color)
@@ -167,14 +132,22 @@ namespace AlpacaIT.VertexTracer
             t2 -= (center2 - t2).normalized * ((1.0f / lightmapSizeMin1) * 4f);
             t3 -= (center2 - t3).normalized * ((1.0f / lightmapSizeMin1) * 4f);
 
-            for (int x = 0; x < lightmapSize; x++)
+            // calculate the bounding box of the polygon in UV space.
+            // we only have to raycast these pixels and can skip the rest.
+            var triangleBoundingBox = ComputeTriangleBoundingBox(t1, t2, t3);
+            var minX = Mathf.FloorToInt(triangleBoundingBox.xMin * lightmapSizeMin1);
+            var minY = Mathf.FloorToInt(triangleBoundingBox.yMin * lightmapSizeMin1);
+            var maxX = Mathf.CeilToInt(triangleBoundingBox.xMax * lightmapSizeMin1);
+            var maxY = Mathf.CeilToInt(triangleBoundingBox.yMax * lightmapSizeMin1);
+
+            for (int x = minX; x < maxX; x++)
             {
-                for (int y = 0; y < lightmapSize; y++)
+                for (int y = minY; y < maxY; y++)
                 {
                     float xx = 0.0f;
                     float yy = 0.0f;
-                    if (x != 0) xx = x / (lightmapSizeMin1);
-                    if (y != 0) yy = y / (lightmapSizeMin1);
+                    if (x != 0) xx = x / lightmapSizeMin1;
+                    if (y != 0) yy = y / lightmapSizeMin1;
 
                     var world = UvTo3d(new Vector2(xx, yy), v1, v2, v3, t1, t2, t3);
                     if (world.Equals(Vector3.zero)) continue;
@@ -270,139 +243,29 @@ namespace AlpacaIT.VertexTracer
             return c1;
         }
 
-        private static (Color c1, Color c2, Color c3) Raycast(Vector3 v1, Vector3 v2, Vector3 v3)
-        {
-            Color c1 = new Color(), c2 = new Color(), c3 = new Color();
-            c1.a = 1f;
-            c2.a = 1f;
-            c3.a = 1f;
-
-            var plane = new Plane(v1, v2, v3);
-            var normal = plane.normal;
-
-            for (int i = 0; i < pointLights.Length; i++)
-            {
-                var pointLight = pointLights[i];
-                var radius = pointLight.lightRadius;
-                if (radius == 0.0f) continue;
-                var position = pointLight.transform.position;
-
-                var v1dir = (position - v1).normalized;
-                if (math.dot(normal, v1dir) < 0f) continue; // early out by normal.
-                var v2dir = (position - v2).normalized;
-                var v3dir = (position - v3).normalized;
-
-                float v1dist = Vector3.Distance(v1, position);
-                float v2dist = Vector3.Distance(v2, position);
-                float v3dist = Vector3.Distance(v3, position);
-
-                // trace from the light to the vertex and check whether we hit close to the vertex.
-                // we check whether the vertex is within the light radius or else skip it.
-                bool v1cast = false;
-                if (v1dist <= radius && Physics.Raycast(position, -v1dir, out var hit1))
-                    v1cast = (Vector3.Distance(hit1.point, v1) < 0.1f);
-
-                bool v2cast = false;
-                if (v2dist <= radius && Physics.Raycast(position, -v2dir, out var hit2))
-                    v2cast = (Vector3.Distance(hit2.point, v2) < 0.1f);
-
-                bool v3cast = false;
-                if (v3dist <= radius && Physics.Raycast(position, -v3dir, out var hit3))
-                    v3cast = (Vector3.Distance(hit3.point, v3) < 0.1f);
-
-                traces += (v1dist <= radius ? 1 : 0);
-                traces += (v2dist <= radius ? 1 : 0);
-                traces += (v3dist <= radius ? 1 : 0);
-
-                float diff1 = math.max(math.dot(normal, v1dir), 0f);
-                float diff2 = math.max(math.dot(normal, v2dir), 0f);
-                float diff3 = math.max(math.dot(normal, v3dir), 0f);
-
-                //Debug.DrawLine(v1 + (normal * 0.01f) + (v1tocenter * 0.01f), v1 + (normal * 0.01f) + (v1tocenter * 0.01f) + (v1dir * v1dist), Color.green, 10f);
-
-                float attenuation1 = math.clamp(1.0f - v1dist * v1dist / (radius * radius), 0.0f, 1.0f) * pointLight.lightIntensity;
-                float attenuation2 = math.clamp(1.0f - v2dist * v2dist / (radius * radius), 0.0f, 1.0f) * pointLight.lightIntensity;
-                float attenuation3 = math.clamp(1.0f - v3dist * v3dist / (radius * radius), 0.0f, 1.0f) * pointLight.lightIntensity;
-
-                if (v1cast)
-                    c1 += attenuation1 * pointLight.lightColor * diff1;
-                if (v2cast)
-                    c2 += attenuation2 * pointLight.lightColor * diff2;
-                if (v3cast)
-                    c3 += attenuation3 * pointLight.lightColor * diff3;
-            }
-
-            for (int i = 0; i < shadowLights.Length; i++)
-            {
-                var pointLight = shadowLights[i];
-                var radius = pointLight.shadowRadius;
-                if (radius == 0.0f) continue;
-                var position = pointLight.transform.position;
-
-                var v1dir = (position - v1).normalized;
-                if (math.dot(normal, v1dir) < 0f) continue; // early out by normal.
-                var v2dir = (position - v2).normalized;
-                var v3dir = (position - v3).normalized;
-
-                float v1dist = Vector3.Distance(v1, position);
-                float v2dist = Vector3.Distance(v2, position);
-                float v3dist = Vector3.Distance(v3, position);
-
-                // trace from the light to the vertex and check whether we hit close to the vertex.
-                // we check whether the vertex is within the light radius or else skip it.
-                bool v1cast = false;
-                if (v1dist <= radius && Physics.Raycast(position, -v1dir, out var hit1))
-                    v1cast = (Vector3.Distance(hit1.point, v1) < 0.1f);
-
-                bool v2cast = false;
-                if (v2dist <= radius && Physics.Raycast(position, -v2dir, out var hit2))
-                    v2cast = (Vector3.Distance(hit2.point, v2) < 0.1f);
-
-                bool v3cast = false;
-                if (v3dist <= radius && Physics.Raycast(position, -v3dir, out var hit3))
-                    v3cast = (Vector3.Distance(hit3.point, v3) < 0.1f);
-
-                traces += 3;
-
-                float diff1 = math.max(math.dot(normal, v1dir), 0f);
-                float diff2 = math.max(math.dot(normal, v2dir), 0f);
-                float diff3 = math.max(math.dot(normal, v3dir), 0f);
-
-                //Debug.DrawLine(v1 + (normal * 0.01f) + (v1tocenter * 0.01f), v1 + (normal * 0.01f) + (v1tocenter * 0.01f) + (v1dir * v1dist), Color.green, 10f);
-
-                float attenuation1 = math.clamp(1.0f - v1dist * v1dist / (radius * radius), 0.0f, 1.0f) * pointLight.shadowIntensity;
-                float attenuation2 = math.clamp(1.0f - v2dist * v2dist / (radius * radius), 0.0f, 1.0f) * pointLight.shadowIntensity;
-                float attenuation3 = math.clamp(1.0f - v3dist * v3dist / (radius * radius), 0.0f, 1.0f) * pointLight.shadowIntensity;
-
-                if (v1cast) c1 = Subtract(c1, attenuation1 * diff1);
-                if (v2cast) c2 = Subtract(c2, attenuation2 * diff2);
-                if (v3cast) c3 = Subtract(c3, attenuation3 * diff3);
-            }
-
-            return (c1, c2, c3);
-        }
-
-        private static Color Subtract(Color a, float b)
-        {
-            return new Color(a.r - b, a.g - b, a.b - b, a.a - b);
-        }
-
-        public static Texture2D toTexture2D(RenderTexture rTex)
-        {
-            Texture2D tex = new Texture2D(256, 256, TextureFormat.RGBA32, false);
-            // ReadPixels looks at the active RenderTexture.
-            RenderTexture.active = rTex;
-            tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
-            tex.Apply();
-            return tex;
-        }
-
         // calculate signed triangle area using a kind of "2D cross product":
         public static float Area(Vector2 p1, Vector2 p2, Vector2 p3)
         {
             var v1 = p1 - p3;
             var v2 = p2 - p3;
             return (v1.x * v2.y - v1.y * v2.x) / 2f;
+        }
+
+        public static Rect ComputeTriangleBoundingBox(Vector2 a, Vector2 b, Vector2 c)
+        {
+            float sx1 = a.x;
+            float sx2 = b.x;
+            float sx3 = c.x;
+            float sy1 = a.y;
+            float sy2 = b.y;
+            float sy3 = c.y;
+
+            float xmax = sx1 > sx2 ? (sx1 > sx3 ? sx1 : sx3) : (sx2 > sx3 ? sx2 : sx3);
+            float ymax = sy1 > sy2 ? (sy1 > sy3 ? sy1 : sy3) : (sy2 > sy3 ? sy2 : sy3);
+            float xmin = sx1 < sx2 ? (sx1 < sx3 ? sx1 : sx3) : (sx2 < sx3 ? sx2 : sx3);
+            float ymin = sy1 < sy2 ? (sy1 < sy3 ? sy1 : sy3) : (sy2 < sy3 ? sy2 : sy3);
+
+            return new Rect(xmin, ymin, xmax - xmin, ymax - ymin);
         }
     }
 }
