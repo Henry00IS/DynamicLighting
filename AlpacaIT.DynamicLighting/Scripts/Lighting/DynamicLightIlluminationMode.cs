cdf19d87ecf0aa8f0fe27b32bc9de8e091de18f8
Henry de Jongh <henry00@live.nl>
2024-10-08 15:30:51 +0200

    Updated the bounce lighting tooltip with more accurate information and tips that reflect the inner workings of the current system.

--- AlpacaIT.DynamicLighting/Scripts/Lighting/DynamicLightIlluminationMode.cs
+++ AlpacaIT.DynamicLighting/Scripts/Lighting/DynamicLightIlluminationMode.cs
@@ -14,19 +14,26 @@ namespace AlpacaIT.DynamicLighting
         DirectIllumination = 0,
 
         /// <summary>
-        /// After calculating direct illumination, performs a single indirect illumination pass
-        /// where directly illuminated texels cast light onto other surfaces. This enhances realism
-        /// by simulating light bounce, providing more natural and detailed lighting effects. This
-        /// method uses 32 bits per texel of memory on individual triangles illuminated by the light.
+        /// After calculating direct illumination, this method performs a single indirect
+        /// illumination pass, where directly illuminated texels cast light onto other surfaces.
+        /// This enhances realism by simulating light bounce, resulting in more natural and detailed
+        /// lighting effects and avoids the black shadows.
         /// <para>
-        /// Indirect illumination can not spread beyond the light radius, it is recommended to use
-        /// this option on important lights with a large radius. The quality of the bounce data
-        /// degrades and blurs over distance, so avoid setting the light radius too large for
-        /// optimal results. Additionally, be aware that this approach will significantly increase
-        /// raytracing times.
+        /// The bounce lighting operates independently of the initial light intensity, meaning you
+        /// can bake it even if the light source is completely dark. The primary factor influencing
+        /// bounce lighting is the light radius, which defines how far the indirect illumination
+        /// spreads. This method requires an additional 8 bits per texel of VRAM on individual
+        /// triangle surfaces receiving bounce light.
+        /// </para>
+        /// <para>
+        /// Indirect illumination can not extend beyond the light radius, so it is recommended to
+        /// apply this technique only to important lights with a substantial radius. The quality of
+        /// the bounce data degrades and blurs over distance, so avoid setting the light radius too
+        /// large for optimal results. Additionally, be aware that this approach may significantly
+        /// increase raytracing times due to the additional calculations involved.
         /// </para>
         /// </summary>
-        [Tooltip("After calculating direct illumination, performs a single indirect illumination pass where directly illuminated texels cast light onto other surfaces. This enhances realism by simulating light bounce, providing more natural and detailed lighting effects. This method uses 32 bits per texel of memory on individual triangles illuminated by the light.\n\nIndirect illumination can not spread beyond the light radius, it is recommended to use this option on important lights with a large radius. The quality of the bounce data degrades and blurs over distance, so avoid setting the light radius too large for optimal results. Additionally, be aware that this approach will significantly increase raytracing times.")]
+        [Tooltip("After calculating direct illumination, this method performs a single indirect illumination pass, where directly illuminated texels cast light onto other surfaces. This enhances realism by simulating light bounce, resulting in more natural and detailed lighting effects and avoids the black shadows.\n\nThe bounce lighting operates independently of the initial light intensity, meaning you can bake it even if the light source is completely dark. The primary factor influencing bounce lighting is the light radius, which defines how far the indirect illumination spreads. This method requires an additional 8 bits per texel of VRAM on individual triangle surfaces receiving bounce light.\n\nIndirect illumination can not extend beyond the light radius, so it is recommended to only apply this technique to important lights with a substantial radius. The quality of the bounce data degrades and blurs over distance, so avoid setting the light radius too large for optimal results. Additionally, be aware that this approach may significantly increase raytracing times due to the additional calculations involved.")]
         SingleBounce = 1,
     }
 }
