497daca48d0ecb6356df55286c293e3b45027f92
Henry de Jongh <henry00@live.nl>
2024-11-28 17:24:14 +0100

    Implemented IPreprocessShaders to strip unlit shader variants.

---
+++ AlpacaIT.DynamicLighting.Editor/Editor/DynamicLightingShaderPreprocessing.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace AlpacaIT.DynamicLighting.Editor
+{
+    /// <summary>Handles removing unnecessary shader variations.</summary>
+    internal class DynamicLightingShaderPreprocessing : IPreprocessShaders
+    {
+        private ShaderKeyword shaderKeywordDynamicLightingQualityLow;
+        private ShaderKeyword shaderKeywordDynamicLightingQualityHigh;
+        private ShaderKeyword shaderKeywordDynamicLightingQualityIntegratedGraphics;
+        private ShaderKeyword shaderKeywordDynamicLightingLit;
+        private ShaderKeyword shaderKeywordDynamicLightingBvh;
+        private ShaderKeyword shaderKeywordDynamicLightingBounce;
+        private ShaderKeyword shaderKeywordDynamicLightingDynamicGeometryDistanceCubes;
+
+        public int callbackOrder => 0;
+
+        public DynamicLightingShaderPreprocessing()
+        {
+            shaderKeywordDynamicLightingQualityLow = new ShaderKeyword("DYNAMIC_LIGHTING_QUALITY_LOW");
+            shaderKeywordDynamicLightingQualityHigh = new ShaderKeyword("DYNAMIC_LIGHTING_QUALITY_HIGH");
+            shaderKeywordDynamicLightingQualityIntegratedGraphics = new ShaderKeyword("DYNAMIC_LIGHTING_INTEGRATED_GRAPHICS");
+            shaderKeywordDynamicLightingLit = new ShaderKeyword("DYNAMIC_LIGHTING_LIT");
+            shaderKeywordDynamicLightingBvh = new ShaderKeyword("DYNAMIC_LIGHTING_BVH");
+            shaderKeywordDynamicLightingBounce = new ShaderKeyword("DYNAMIC_LIGHTING_BOUNCE");
+            shaderKeywordDynamicLightingDynamicGeometryDistanceCubes = new ShaderKeyword("DYNAMIC_LIGHTING_DYNAMIC_GEOMETRY_DISTANCE_CUBES");
+        }
+
+        public void OnProcessShader(Shader shader, ShaderSnippetData snippet, IList<ShaderCompilerData> data)
+        {
+            var removed = 0;
+
+            var dataCount = data.Count;
+            for (int i = dataCount; i-- > 0;)
+            {
+                var keywords = data[i].shaderKeywordSet;
+
+                // the unlit mode has many variations that can be stripped from the build:
+                if (keywords.IsDisabled(shaderKeywordDynamicLightingLit))
+                {
+                    if (keywords.AnyEnabled(
+                        shaderKeywordDynamicLightingBvh,
+                        shaderKeywordDynamicLightingQualityLow,
+                        shaderKeywordDynamicLightingQualityHigh,
+                        shaderKeywordDynamicLightingQualityIntegratedGraphics,
+                        shaderKeywordDynamicLightingBvh,
+                        shaderKeywordDynamicLightingBounce,
+                        shaderKeywordDynamicLightingDynamicGeometryDistanceCubes
+                    ))
+                    {
+                        removed++;
+                        data.RemoveAt(i);
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AlpacaIT.DynamicLighting.Editor/Editor/Utilities/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.Rendering;
+
+namespace AlpacaIT.DynamicLighting.Editor
+{
+    internal static class Extensions
+    {
+        /// <summary>Checks whether the keyword is disabled.</summary>
+        /// <param name="shaderKeywordSet">The keyword set to be checked.</param>
+        /// <param name="keyword">The keyword to find.</param>
+        /// <returns>True when the keyword is disabled else false.</returns>
+        public static bool IsDisabled(this ShaderKeywordSet shaderKeywordSet, ShaderKeyword keyword)
+        {
+            return !shaderKeywordSet.IsEnabled(keyword);
+        }
+
+        /// <summary>Checks whether all keywords are enabled.</summary>
+        /// <param name="shaderKeywordSet">The keyword set to be checked.</param>
+        /// <param name="keywords">The keywords to match.</param>
+        /// <returns>True when all keywords are enabled else false.</returns>
+        public static bool AllEnabled(this ShaderKeywordSet shaderKeywordSet, params ShaderKeyword[] keywords)
+        {
+            for (int i = 0; i < keywords.Length; i++)
+                if (!shaderKeywordSet.IsEnabled(keywords[i]))
+                    return false;
+            return true;
+        }
+
+        /// <summary>Checks whether any keyword is enabled.</summary>
+        /// <param name="shaderKeywordSet">The keyword set to be checked.</param>
+        /// <param name="keywords">The keywords to match.</param>
+        /// <returns>True when any of the keywords are enabled else false.</returns>
+        public static bool AnyEnabled(this ShaderKeywordSet shaderKeywordSet, params ShaderKeyword[] keywords)
+        {
+            for (int i = 0; i < keywords.Length; i++)
+                if (shaderKeywordSet.IsEnabled(keywords[i]))
+                    return true;
+            return false;
+        }
+    }
+}
